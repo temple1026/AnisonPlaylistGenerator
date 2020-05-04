ffa264812f3a9ab80bdac6a20ed9245f04635b91
temple1026 <taka.ambasa@gmail.com>
2020-05-05 00:00:40 +0900

    Code cleanup

--- src/apg.py
+++ src/apg.py
@@ -66,14 +66,12 @@ class APG():
             
 
     def makeAnisonDatabase(self, path_data):
-        # artist_type = ["歌手", "作詞", "作曲"]
-        # music_type = ["OP", "ED", "IM", "IN"]
-        
         data_name = ["anison.csv", "game.csv", "sf.csv"]
         file_paths = [i for i in glob.glob(path_data + "/**", recursive=True) if os.path.splitext(i)[-1] == ".csv"]
 
         if os.path.exists(self.path_database):
             os.remove(self.path_database)
+
         self.initDatabase()
 
         for file_path in file_paths:
@@ -110,44 +108,7 @@ class APG():
                     cursor.execute("INSERT OR IGNORE INTO relation_songs(id_anime, id_song, num_order, id_type) VALUES (?, ?, ?, ?)", (id_anime, id_song, _order, id_type,))
 
                 con.commit()
-                
-        # for file_path in file_paths:
-        #     if os.path.basename(file_path) in data_name:
-        #         category = os.path.splitext(os.path.basename(file_path))[0]
-        #         with sqlite3.connect(self.path_database)as con, open(file_path, "r", encoding="utf-8") as f:
-        #             cursor = con.cursor()
-        #             cursor.execute("CREATE TABLE IF NOT EXISTS '%s'('%s', '%s', '%s', '%s', '%s', '%s')" % (category, "artist", "title", "anime", "genre", "oped", "order"))
 
-        #             command = "INSERT INTO " + category + " VALUES(?, ?, ?, ?, ?, ?)"
-                    
-        #             lines = f.readlines()
-        #             buffer = []
-        #             buffer_size = 1000
-
-        #             for i, line in tqdm(enumerate(lines[1:])):
-        #                 if not self.run:
-        #                     break
-                        
-        #                 self.prog_db = int((i + 1)/len(lines)*100)
-            
-        #                 *keys, = line.split(",")
-        #                 artist, title, order, oped, anime, genre = trim(keys[7]), trim(keys[6]), trim(keys[4]), trim(keys[3]), trim(keys[2]), trim(keys[1]) 
-                        
-        #                 buffer.append([artist, title, anime, genre, oped, order])
-
-        #                 if i%buffer_size == 0 or i == len(lines) - 1:
-        #                     cursor.executemany(command, buffer)
-        #                     buffer = []
-                
-        #             # Delete the duplication of the database
-        #             cursor.executescript("""
-        #                 CREATE TEMPORARY TABLE tmp AS SELECT DISTINCT * FROM """ + category + """;
-        #                 DELETE FROM """ + category + """;
-        #                 INSERT INTO """ + category + """ SELECT * FROM tmp;
-        #                 DROP TABLE tmp;
-        #                 """)
-
-        #             con.commit()
             self.prog_db = 0
 
     def getMusicInfo(self, path):
@@ -209,7 +170,7 @@ class APG():
                     id_local_file = cursor.execute("SELECT id_local_file FROM local_files WHERE path_file = ? ", (music_file,)).fetchall()[0][0]
                     
                     cursor.execute("INSERT OR IGNORE INTO local_musics(id_local_artist, id_local_file) VALUES (?, ?)", (id_local_artist, id_local_file,)).lastrowid
-                    # print(artist, id_local_artist, id_local_file)
+
             con.commit()
 
     def getInfoDB(self, command, cursor):
@@ -238,11 +199,16 @@ class APG():
                 return
 
             target_category = target_category[:target_category.rfind(" OR ")]
+            # # name_anime LIKE '%ｶﾞﾝﾀﾞﾑ%'
+            # keyword = trim("ガンダム")
+            # use_key = True
+            # phrase = {True:"AND (name_anime LIKE '%%%s%%')"%keyword, False:""}
+            # print(phrase[use_key])
 
             cursor.executescript(
                 """
                 -- カテゴリに対応するアニメを抽出
-                CREATE TEMPORARY TABLE tmp AS SELECT id_anime, name_anime FROM animes INNER JOIN (SELECT id_category FROM categories WHERE (""" + target_category + """)) AS cat_id ON cat_id.id_category = animes.id_category;
+                CREATE TEMPORARY TABLE tmp AS SELECT id_anime, name_anime FROM animes INNER JOIN (SELECT id_category FROM categories WHERE """ + target_category + """) AS cat_id ON cat_id.id_category = animes.id_category;
                 -- アニメに対応する楽曲を抽出
                 CREATE TEMPORARY TABLE tmp1 AS SELECT id_song FROM relation_songs INNER JOIN tmp ON tmp.id_anime = relation_songs.id_anime;
                 -- アニソンに対応するアーティストidを抽出
@@ -253,77 +219,21 @@ class APG():
                 CREATE TEMPORARY TABLE tmp4 AS SELECT DISTINCT id_artist, name_artist, id_local_artist, name_local_artist FROM local_artists INNER JOIN tmp3 ON local_artists.name_local_artist LIKE '%'||tmp3.name_artist||'%';
                 -- アーティスト名のないレコードを削除
                 DELETE FROM tmp4 WHERE name_artist = '';
-                
+
+                -- 該当するローカルアーティストのファイルを取得
                 CREATE TEMPORARY TABLE tmp5 AS SELECT id_local_file, name_artist FROM local_musics INNER JOIN tmp4 ON tmp4.id_local_artist = local_musics.id_local_artist;
+                -- ファイルから曲などの情報を取得
                 CREATE TEMPORARY TABLE tmp6 AS SELECT DISTINCT name_artist, name_song, length_file, path_file FROM local_files INNER JOIN tmp5 ON tmp5.id_local_file = local_files.id_local_file;
+                -- アニソンアーティストの曲を取得
                 CREATE TEMPORARY TABLE tmp7 AS SELECT name_song, name_artist FROM songs INNER JOIN tmp4 ON tmp4.id_artist = songs.id_artist; 
+                -- 一致する曲を取得
                 CREATE TEMPORARY TABLE tmp8 AS SELECT DISTINCT tmp6.name_song, length_file, path_file FROM tmp6 INNER JOIN tmp7 ON ((tmp6.name_song LIKE '%'||tmp7.name_song||'%') AND (tmp6.name_artist = tmp7.name_artist)); 
                 """
             )
 
-            # cursor.execute("SELECT length_file, name_song, path_file FROM tmp8")
-            # cursor.execute("SELECT * FROM tmp8 ORDER BY name_song DESC LIMIT 20")
-            # cursor.execute("SELECT * FROM tmp5")
-            # cursor.execute("SELECT * FROM local_artists ORDER BY name_local_artist")
             cursor.execute("SELECT * FROM tmp8")
             lines = cursor.fetchall()
             lines = [["#EXTINF: " + str(int(line[1])) + ", " + line[0] + "\n" + line[2]] for line in lines]
-
-            print(lines)
-
-            # cursor.execute("SELECT * FROM tmp4")
-            # cursor.execute("SELECT * FROM local_artists ORDER BY name_local_artist")
-
-            # result = cursor.fetchall()
-            # for idx, r in enumerate(result):
-            #     print(idx, r)
-            # # print(result)
-
-            # cursor.execute("SELECT id_anime FROM animes INNER JOIN categories ON animes.id_category = (SELECT id_category FROM categories WHERE name_category = \'" + category + "\');")
-                
-            # for category in categories:
-            #     if not check_categories[category]:
-            #         continue
-
-            #     if use_key:
-            #         cursor.execute("SELECT DISTINCT artist FROM '%s' WHERE anime LIKE \'%%%s%%\'" % (category, trim(keyword)))
-            #     else:
-            #         cursor.execute("SELECT DISTINCT artist FROM '%s'" % category)
-
-            #     artist_db = cursor.fetchall()
-
-            #     # Get all musics corresponding to aquired artist, then apply flatten
-            #     artist_lib = sorted(set([artist[0] for artist in self.getInfoDB('SELECT artist FROM library', cursor)]))
-
-            #     for i, artist in tqdm(enumerate(artist_lib)):
-            #         if not self.run:
-            #             break
-
-            #         self.prog_playlist = int((i + 1)/len(artist_lib)*100)
-
-            #         similarities = [difflib.SequenceMatcher(None, artist, a[0]).ratio() for a in artist_db]
-                    
-            #         if 0 < len(similarities) and th_artist < max(similarities):
-            #             info_list = self.getInfoDB('SELECT * FROM library WHERE artist LIKE \'' + artist + '\'', cursor)
-
-            #             if use_key:
-            #                 cursor.execute("SELECT DISTINCT * FROM '%s' WHERE artist LIKE \'%%%s%%\' AND anime LIKE \'%%%s%%\'" % (category, artist_db[similarities.index(max(similarities))][0], trim(keyword)))
-            #             else:
-            #                 cursor.execute("SELECT DISTINCT * FROM '%s' WHERE artist LIKE \'%%%s%%\'" % (category, artist_db[similarities.index(max(similarities))][0]))
-
-            #             title_list = cursor.fetchall()
-
-            #             for info in info_list:
-            #                 artist, title, length, path = info[0], info[1], info[2], info[3]                         
-
-            #                 title_ratio = [difflib.SequenceMatcher(None, title, t[1]).ratio() for t in title_list]                
-                            
-            #                 if len(title_ratio) > 0 and th_title < max(title_ratio):
-            #                     if duplication or (sum([(title in line[0]) for line in lines]) <= 0):
-            #                         t = title_list[title_ratio.index(max(title_ratio))]
-            #                         lines.append(['#EXTINF: ' + str(int(length)) + ', ' + title + "\n" + path, t[-1]])
-            
-            # lines = sorted(lines, key=lambda x:x[1])
             pl.writelines('#EXTM3U \n')
             pl.writelines("\n".join([line[0] for line in lines]))
 
@@ -341,11 +251,11 @@ def run(path_config='./config.ini'):
     print(path_database)
     gen = APG(path_database)
     
-    # print("Adding anison information to the database. (1/3)")
-    # gen.makeAnisonDatabase(path_data)
+    print("Adding anison information to the database. (1/3)")
+    gen.makeAnisonDatabase(path_data)
 
-    # print("Adding lirary information to the database. (2/3)")
-    # gen.makeLibrary(path_music)
+    print("Adding lirary information to the database. (2/3)")
+    gen.makeLibrary(path_music)
 
     print("Making playlist. (3/3)")
     gen.generatePlaylist(path_playlist)
